28ce0e189fbd53fefada6834d4116fbceda8ac2f
robwid-9 <robwid-9@student.ltu.se>
2020-12-08 13:27:18 +0100

    commit

---
+++ file.js
@@ -0,0 +1,28 @@
+﻿const PDFStart = nameRoute => { }
+const startPdf = () => {
+    PDFStart('../test.pdf')
+}
+window.addEventListener('load', startPdf);
+let loadingTask = pdfjsLib.getDocument(nameRoute),
+    pdfDoc = null,
+    canvas = document.querySelector('#cnv'),
+    ctx = canvas.getContext('2d'),
+    scale = 1.5,
+    numPage = 1;
+loadingTask.promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+    document.querySelector('#npages').innerHTML = pdfDoc.numPages;
+    GeneratePDF(numPage)
+});
+const GeneratePDF = numPage => {
+    pdfDoc.getPage(numPage).then(page => {
+        let viewport = page.getViewport({ scale: scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        let renderContext = {
+            canvasContext: ctx, viewport: viewport
+        }
+        page.render(renderContext);
+    })
+    document.querySelector('#npages').innerHTML = numPage;
+}
---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+     <meta charset="utf-8" />
+    <link rel="stylesheet" href="app.css" type="text/css" />
+    <?php
+    if(isset($_GET['submit']))
+    {
+
+    // Count total files
+    $countfiles = count($_FILES['file']['name']);
+
+    // Looping all files
+    for($i = 0; $i<$countfiles; $i++)
+                  {
+                  $filename = $_FILES['file']['name'][$i];
+                  move_uploaded_file($_FILES['file']['tmp_name'][$i],'upload/'.$filename);
+                  }
+                  ?>
+</head>
+<body>
+
+  <form method='get' action='' enctype='multipart/form-data'>
+        <input type="file" name="file[]" id="file" multiple>
+        <input type='submit' name='submit' value='Upload'>
+   </form>
+
+
+</body>
+</html>
